062240330d0c6c77c817f1f12438019a0b4b1923
Angela <yaoliu928@gmail.com>
2024-04-18 15:26:25 +1000

    enable formatResponse and notFound middleware

--- src/app.js
+++ src/app.js
@@ -3,14 +3,20 @@ const v1Router = require('./routes');
 const config = require('./config');
 const getLogger = require('./common/logger');
 const morgan = require('./common/morgan');
+const formatResponseMiddleware = require('./middleware/formatResponse.middleware');
+const notFoundMiddleware = require('./middleware/notFound.middleware');
 
 const logger = getLogger(__filename);
 
 const app = express();
 
+app.use(formatResponseMiddleware);
+
 app.use(morgan);
 
 app.use('/v1', v1Router);
+
+app.use(notFoundMiddleware);
 
 app.listen(config.PORT, () => {
   logger.info(`Server is listening on port ${config.PORT}.`);
