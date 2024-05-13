93ab649d370cc4fe156bf946042a557b844ccbed
Angela <yaoliu928@gmail.com>
2024-05-13 19:33:26 +1000

    Revert "solve conflict"
    This reverts commit cf34b7071faf0dceebf8586ad3dda1e1f70aa33f.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const studentRouter = require('./student.router');
+<<<<<<< HEAD
 const courseRouter = require('./course.router');
 const authRouter = require('./auth.router');
 const authGuardMiddleware = require('../middleware/authGuard.middleware');
@@ -9,5 +10,11 @@ const v1Router = Router();
 v1Router.use('/students', authGuardMiddleware, studentRouter);
 v1Router.use('/courses', authGuardMiddleware, courseRouter);
 v1Router.use('/auth', authRouter);
+=======
+
+const v1Router = Router();
+
+v1Router.use('/students', studentRouter);
+>>>>>>> master
 
 module.exports = v1Router;
